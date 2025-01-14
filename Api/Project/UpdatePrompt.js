6318b0dd640f18d6b4887c577e3346151f7917fa
aid003 <teremochek859@gmail.com>
2025-01-15 00:13:44 +0300

    scscsc

--- Api/Project/UpdatePrompt.js
+++ Api/Project/UpdatePrompt.js
@@ -2,11 +2,13 @@ import asyncHandler from "express-async-handler";
 import { prisma } from "../../index.js";
 
 export const updatePrompt = asyncHandler(async (req, res) => {
-  const { projectId, type, text } = req.body;
+  const { projectId, type, text, id } = req.body;
+
+  console.log(text)
 
   try {
     const prompt = await prisma.prompts.update({
-      where: { id: Number(projectId), type },
+      where: { projectId: Number(projectId), type, id },
       data: { content: text },
     });
 
